dd0e567edc8cb42f9c11024d7b77f3b9f59d7706
Christopher Rigor <crigor@gmail.com>
2011-01-01 13:38:53 +0800

    new rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resqueapp::Application.config.secret_token = '5cea687f2374d487048fd44821abb53eeafea97cc11bcb3104eb122c72faa529dfc3ee7fe4e396a6baa5861c3b3d1218d3c13e048f2729782a817ae313e60152'
