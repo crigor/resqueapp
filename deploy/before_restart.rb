c5f6ac23907677476212eb96a20ed053249b98b2
Christopher Rigor <crigor@gmail.com>
2012-03-12 14:34:14 +0800

    sed

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,2 @@
-run "sed -i 's/URL/http:\/\/localhost:3000\/foo/g' #{release_path}/lib/test_url.rb"
+run "sed -i.BK 's/URL/foo/g' #{release_path}/lib/test_url.rb"
+#run "sed -i.BK 's/URL/http:\/\/localhost:3000\/foo/g' #{release_path}/lib/test_url.rb"
