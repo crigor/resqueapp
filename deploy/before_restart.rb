28c145d6fcd41959b6ca4a95a4b7eb559c80acc1
Christopher Rigor <crigor@gmail.com>
2012-03-12 14:39:31 +0800

    remove extra /

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,3 +1,3 @@
 #run "sed -i.BK 's/URL/foo/g' #{release_path}/lib/test_url.rb"
-run "sed -i.BK 's/URL/http\/\//foo/g' #{release_path}/lib/test_url.rb"
+run "sed -i.BK 's/URL/http\/\//g' #{release_path}/lib/test_url.rb"
 #run "sed -i.BK 's/URL/http:\/\/localhost:3000\/foo/g' #{release_path}/lib/test_url.rb"
