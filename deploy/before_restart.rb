e9d939bb492da80ff9c1cb2e5b915b01a2476ea1
Christopher Rigor <crigor@gmail.com>
2012-03-27 13:50:37 -0700

    add a method on the deploy hook

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,5 +1,10 @@
 #run "sed -i.BK 's/URL/foo/g' #{release_path}/lib/test_url.rb"
-run "sed -i.BK 's/URL/http\\/\\//g' #{release_path}/lib/test_url.rb"
-raise "error on deploy hook"
+#raise "error on deploy hook"
 #run "sed -i.BK 's/URL/http:\/\/localhost:3000\/foo/g' #{release_path}/lib/test_url.rb"
-#foo
+
+def foobar
+  run "sed -i.BK 's/URL/http\\/\\//g' #{release_path}/lib/test_url.rb"
+end
+
+foobar
+
